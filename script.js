913d8db4ed8d83a3cac690ca6e7c023fbd25f813
Kacper Zacharski <kacper@zacharski.hub.pl>
2021-01-27 23:44:13 +0100

    Files

---
+++ script.js
@@ -0,0 +1,29 @@
+function classic() {
+    location.href = 'ipodclassic/index.html';
+} function back() {
+    location.href = '../index.html';
+} function nano() {
+    location.href = 'ipodnano/index.html';
+} function shuffle() {
+    location.href = 'ipodshuffle/index.html'
+} function about() {
+    customAlert.alert("Website created by Kacper Zacharski. iPod, iTunes and Apple are trademarks of Apple Inc.")
+} function classicfirst() {
+    location.href = 'firstgen.html'
+} function backclassic() {
+    location.href = 'index.html'
+} function classicsecond() {
+    location.href = 'secondgen.html'
+} function classicthird() {
+    location.href = 'thirdgen.html'
+} function classicfourth() {
+    location.href = 'fourthgen.html'
+} function classicfifth() {
+    location.href = 'fifthgen.html'
+} function classicsixth() {
+    location.href = 'sixthgen.html'
+} function touch() {
+    location.href = 'ipodtouch/index.html'
+} function nanofirst() {
+    location.href = 'firstgen.html'
+}
